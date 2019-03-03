fabb3328487b1ba0301a1121a0a73f97811fd03b
JesseJordan14 <45674954+JesseJordan14@users.noreply.github.com>
2019-03-02 17:55:09 -0700

    Implemented Edge and Vertex classes

--- src/assign07/Edge.java
+++ src/assign07/Edge.java
@@ -2,4 +2,14 @@ package assign07;
 
 public class Edge {
 
+	// destination of this directed edge
+	private Vertex dst;
+	
+	public Edge(Vertex dst) {
+		this.dst = dst;
+	}
+	
+	public Vertex getOtherVertex() {
+		return this.dst;
+	}
 }
--- src/assign07/Vertex.java
+++ src/assign07/Vertex.java
@@ -1,5 +1,66 @@
 package assign07;
 
-public class Vertex {
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
+public class Vertex<E> implements Comparator<Vertex>{
+
+	private E element;
+	
+	private boolean visited;
+	private Vertex previous;
+	private int distanceFromStart;
+	
+	// adjacency list
+	private LinkedList<Edge> adj;
+	
+	public Vertex(E element) {
+		this.element = element;
+		this.adj = new LinkedList<Edge>();
+	}
+	
+	public E getElement() {
+		return element;
+	}
+	
+	public void addEdge(Vertex otherVertex) {
+		adj.add(new Edge(otherVertex));
+	}
+	
+	public Iterator<Edge> edges() {
+		return adj.iterator();
+	}
+	
+	public void setDistanceFromStart(int d) {
+		distanceFromStart = d;
+	}
+	
+	public int getDistanceFromStart() {
+		return distanceFromStart;
+	}
+	
+	public LinkedList<Edge> getEdges() {
+		return adj;
+	}
+	
+	public boolean getVisited() {
+		return visited;
+	}
+	
+	public void setPrevious(Vertex x) {
+		previous = x;
+	}
+	
+	public Vertex getPrevious() {
+		return previous;
+	}
+	
+	@Override
+	public int compare(Vertex o1, Vertex o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
