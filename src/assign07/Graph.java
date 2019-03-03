6b40069292102217cd3b200d3c9297c2dc1e2cfa
DallenBurwell <dallen.burwell@gmail.com>
2019-03-02 18:36:26 -0700

    addEdge Method Created for Graph

--- src/assign07/Graph.java
+++ src/assign07/Graph.java
@@ -22,6 +22,27 @@ public class Graph<E> implements Comparator<E>{
 		vertices = new HashMap<E, Vertex>();
 	}
 	
+	public void addEdge(E element1, E element2) {
+		
+		Vertex vertex1;
+		if(vertices.containsKey(element1)) {
+			vertex1 = vertices.get(element1);
+		}
+		else {
+			vertex1 = new Vertex(element1);
+			vertices.put(element1, vertex1);
+		}
+		
+		Vertex vertex2;
+		if(vertices.containsKey(element2)) {
+			vertex2 = vertices.get(element2);
+		}
+		else {
+			vertex2 = new Vertex(element2);
+			vertices.put(element2, vertex2);
+		}
+		vertex1.addEdge(vertex2);
+	}
 	
 	@Override
 	public int compare(E o1, E o2) { 
